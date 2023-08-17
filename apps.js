c96f984909abe259b1f0d984cafa8496d6deae08
Buzo06 <i.rodriguezbuzo@ugto.mx>
2023-08-17 08:45:35 -0600

    agregamos cowsay

--- apps.js
+++ apps.js
@@ -1,5 +1,6 @@
-let fruta= ('pera','fresa','manzana', 'mango', 'papaya')
+const {fruits, money}= required('./frutas')
 
 frutas.forEach(fruta => {
     console.log('La fruta es: ', fruta)
 });
+console.log('Dinero: ', money)
---
+++ frutas.js
@@ -0,0 +1,7 @@
+let fruta= ('pera','fresa','manzana', 'mango', 'papaya')
+const money = 5000
+
+mobile.exports= {
+    frutas,
+    money
+};
